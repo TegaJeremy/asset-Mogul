f5ea183786a7111b513a57286ec23d6d478f71b7
Agbo-Emmanuel <agboe4102@gmail.com>
2024-09-08 00:31:48 +0100

    hh

--- src/dashboard/userDashboard/GeneralNumbers.jsx
+++ src/dashboard/userDashboard/GeneralNumbers.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './GeneralNumbers.css'
 import { FaUserGroup } from "react-icons/fa6";
 import { MdTrendingUp } from 'react-icons/md';
 import { RiBankLine } from 'react-icons/ri';
 
 const GeneralNumbers = () => {
+
+    const [allUsers, setAllUsers] = useState(0)
+    const [kycCount, setKycCount] = useState(0)
+    const [totalDeposit, setTotalDeposit] = useState(0)
+
+    const getAllUsers = ()=>{
+
+        fetch('https://asset-mogul-back.onrender.com/getAllUsersCount', { method: 'GET' })
+            .then(response => response.json())
+            .then(data => setAllUsers(data.totalUsers))
+            .catch(error => console.error('Error:', error));
+
+    }
+
+    const getKycCount = ()=>{
+
+        fetch('https://asset-mogul-back.onrender.com/unverifiedCount', { method: 'GET' })
+            .then(response => response.json())
+            .then(data => setKycCount(data.count))
+            .catch(error => console.error('Error:', error));
+
+    }
+
+    const getTotalDeposit = ()=>{
+
+        fetch('https://asset-mogul-back.onrender.com/unverifiedCount', { method: 'GET' })
+            .then(response => response.json())
+            .then(data => setTotalDeposit(data.count))
+            .catch(error => console.error('Error:', error));
+
+    }
+      
+    useEffect(()=>{
+
+        getAllUsers();
+        getKycCount()
+        getTotalDeposit()
+
+    },[])
 
   return (
 
@@ -17,16 +56,7 @@ const GeneralNumbers = () => {
                 </div>
                 <div className='general_numbers_text'>
                     <p>Total Users</p>
-                    <h3>10</h3>
-                </div>
-            </div>
-            <div className='general_numbers_container'>
-                <div className='general_numbers_icon_container'>
-                    <MdTrendingUp/>
-                </div>
-                <div className='general_numbers_text'>
-                    <p>Total Profit</p>
-                    <h3>$20,000</h3>
+                    <h3>{ allUsers }</h3>
                 </div>
             </div>
             <div className='general_numbers_container'>
@@ -35,7 +65,16 @@ const GeneralNumbers = () => {
                 </div>
                 <div className='general_numbers_text'>
                     <p>Total Deposit</p>
-                    <h3>$20,000</h3>
+                    <h3>${ totalDeposit }</h3>
+                </div>
+            </div>
+            <div className='general_numbers_container'>
+                <div className='general_numbers_icon_container'>
+                    <MdTrendingUp/>
+                </div>
+                <div className='general_numbers_text'>
+                    <p>Total kyc count</p>
+                    <h3>{ kycCount }</h3>
                 </div>
             </div>
         </div>
