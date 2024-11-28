83d7ab6fea8b1721197cab90427638de02d23119
Agbo-Emmanuel <agboe4102@gmail.com>
2024-11-28 19:43:54 +0100

    hgf

--- src/dashboard/withdraw/Withdraw.jsx
+++ src/dashboard/withdraw/Withdraw.jsx
@@ -69,6 +69,7 @@ const Withdraw = () => {
       toast.success(response?.data?.message);
       toast.success(`Remaining balance: $${response?.data?.remainingBalance}`);
       setLoading(false);
+      nav("/dashboard")
     } catch (err) {
       console.log(err);
       setLoading(false);
